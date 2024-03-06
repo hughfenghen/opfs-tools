ab0e61b45f3a73b6fbba81ed45ee0278c6951e3f
hughfenghen <hughfenghen@gmail.com>
2024-03-06 11:56:39 +0800

    feat: benchmark for node.js

---
+++ demo/nodejs-rw-benchmark.js
@@ -0,0 +1,30 @@
+import fs from 'fs'
+
+const fileName = './testfile'
+fs.unlinkSync(fileName)
+
+const writeData = Array(1000)
+  .fill(true)
+  .map(() => new Uint8Array(Array(1024 * 100).fill(1)));
+
+console.time('write 100M')
+for (const d of writeData) {
+  fs.appendFileSync(fileName, d)
+}
+console.timeEnd('write 100M')
+
+
+const startPoints = Array(10000)
+  .fill(true)
+  .map(() => ~~(Math.random() * 1e5));
+
+console.time('read 100k, 10000 times')
+const fd = fs.openSync(fileName, 'r');
+for (const p of startPoints) {
+  fs.readSync(fd, Buffer.alloc(100 * 1024), { offset: p })
+}
+console.timeEnd('read 100k, 10000 times')
+
+console.time('read all 100M')
+fs.readFileSync(fileName)
+console.timeEnd('read all 100M')
