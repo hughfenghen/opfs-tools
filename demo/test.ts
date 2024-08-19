2f2a6931367c914db3bef30c1d0eda3e0a5ed04a
hughfenghen <hughfenghen@gmail.com>
2024-08-19 15:00:14 +0800

    chroe: test on safari

--- demo/test.ts
+++ demo/test.ts
@@ -1,4 +1,4 @@
-import { tmpfile, write } from '../src';
+import { tmpfile, file, write } from '../src';
 
 console.log(111, localStorage.getItem('OPFS_TOOLS_EXPIRES_TMP_FILES'));
 const f = tmpfile();
@@ -6,5 +6,14 @@ console.log(222, f.path);
 await write(f, '111111111');
 
 await write('/.opfs-tools-temp-dir/xxx', '2222');
+
+const filePath = '/unit-test/file';
+const f1 = file(filePath, 'rw-unsafe');
+const f2 = file(filePath, 'rw-unsafe');
+
+await write(f1, '111');
+await write(f1, '222');
+
+console.log('多文件句柄读写', (await f2.text()) === '222');
 
 export {};
